b523733bd87d0ee6d0e64f68678e89d1553efaf9
Justin Zhu <justinthezhu@gmail.com>
2020-05-30 11:07:57 -0700

    Added remote to project

---
+++ app.py
@@ -0,0 +1,16 @@
+# import the model
+import tkinter as tk
+
+# m is the main window object
+m = tk.Tk()
+# Code to add widgets will go here...
+
+mainWindowTitle = tk.Label(m, text="Our New GUI Hi")
+mainWindowTitle.pack()
+m.mainloop()
+
+# mainWindow = Tk()
+# mainWindowTitle = Label(mainWindow, text="Our New GUI")
+# mainWindowTitle.pack()
+# mainWindow.mainloop()
+
